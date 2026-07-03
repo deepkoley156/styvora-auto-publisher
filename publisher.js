7d589a145b1851cb676d113c0bf3fc0f3e8ad479
deepkoley156 <deepkoley156@gmail.com>
2026-07-04 00:06:27 +0530

    Update publisher.js

--- publisher.js
+++ publisher.js
@@ -9,6 +9,15 @@ function buildHtml(title, desc, affiliateLink, imageUrl, hashtags) {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6GVYZCMMGH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6GVYZCMMGH');
+</script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title} | Styvora Fashion</title>
